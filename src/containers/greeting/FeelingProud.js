20e1b43daea6eea6d6db076505270a1831cc7290
rajnandini1900 <tanmayc2001@gmail.com>
2024-03-15 23:05:33 +0530

    First Commit

---
+++ src/containers/greeting/FeelingProud.js
@@ -0,0 +1,10 @@
+import React from "react";
+import portfolioPhoto from "../../assets/images/portfolio.jpg";
+import "./FeelingProud.css";
+
+function FeelingProud(props) {
+  // const theme = props.theme;
+  return <img src={portfolioPhoto} alt="portfolio-profile" />;
+}
+
+export default FeelingProud;
---
+++ src/portfolio.js
@@ -0,0 +1,588 @@
+const settings = {
+  isSplash: true, 
+  useCustomCursor: true, 
+};
+
+const greeting = {
+  title: "Hi",
+  title2: " I'm Sanskriti Sharma",
+  logo_name: "Sanskriti Sharma",
+  full_name: "Sanskriti Sharma",
+  subTitle1: "Product Manager.",
+  subTitle2: "Data Analyst.",
+  subTitle: "Full Stack Developer, Life Long Learner.",
+  resumeLink:
+    "",
+  mail: "",
+};
+
+const socialMediaLinks = {
+  github: "https://github.com/sanskritiiiiii",
+  linkedin: "https://www.linkedin.com/in/sanskriti-sharma-75766419a/",
+  gmail: "sanskriti.22011160@viit.ac.in",
+  twitter: "https://twitter.com/Sanskriti_31",
+};
+
+const skills = {
+  data: [
+    {
+      _id: "354c54b7-ffd8-40f1-972f-1d006733df25",
+      title: "Data Analyst",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Basic Web Development Skills for developing frontend applications",
+        "⚡ Data Analysis using python",
+        "⚡ Understanding Python and its related libraries",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "logos:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "Tailwind CSS",
+          fontAwesomeClassname: "logos:tailwindcss-icon",
+          style: {
+            color: "#635BFF",
+            width: "1em",
+            height: "1em",
+          },
+        },
+        {
+          skillName: "Bootstrap",
+          fontAwesomeClassname: "simple-icons:bootstrap",
+          style: {
+            color: "#563d7c",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      _id: "c026fe64-1542-42b0-84ab-9311840c169f",
+      title: "Management",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience in field of Management",
+        "⚡ Good Team Player , good with technicalities",
+        "⚡ Good programming skills+",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Google Cloud Platform",
+          fontAwesomeClassname: "skill-icons:gcp-dark",
+          style: {
+            backgroundColor: "transparent",
+            width: "1em",
+            height: "1em",
+          },
+        },
+        {
+          skillName: "Vercel",
+          fontAwesomeClassname: "skill-icons:vercel-light",
+          style: {
+            color: "gray",
+            fill: "gray",
+            width: "1em",
+            height: "1em",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+            width: "1em",
+            height: "1em",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          width: "1em",
+            height: "1em",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+            width: "1em",
+            height: "1em",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+            width: "1em",
+            height: "1em",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+            width: "1em",
+            height: "1em",
+          },
+        },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+            width: "1em",
+            height: "1em",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Vishwakarma Institute Of Information Technology",
+      subtitle: "BTECH in Electronics and Telecommunication Engineering(ENTC)",
+      logo_path: "viit.jpg",
+      alt_name: "VIIT",
+      duration: "2021 - 2024",
+      descriptions: [
+        "⚡ Vice-President at EDC Cell ",
+        "⚡ Taken Various Courses related to electronics.",
+      ],
+      website_link: "https://www.viit.ac.in/",
+    },
+    {
+      title: "FITJEE",
+      subtitle: "Higher Sedondary Education",
+      logo_path: "fitjee.png",
+      alt_name: "GGSIPU",
+      duration: "2018 - 2021",
+      descriptions: [
+        "⚡ Explored Physics,Chemistry,Maths in greater Detail",
+      ],
+      website_link: "https://www.fiitjee.com/",
+    },
+    {
+      title: "Chavara English Medium High School Shahada",
+      subtitle: "10th Grade",
+      logo_path: "chavara.png",
+      alt_name: "GGSIPU",
+      duration: "2017 - 2018",
+      descriptions: [
+        "⚡ Head Girl of the School",
+        "⚡ Won various Awards in Debate and Elocution Competitions"
+      ],
+      website_link: "https://schools.org.in/nandurbar/27010319230/chavara-english-m-school-shahada.html",
+    }
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "OnPremise Sales Virtual Program",
+      subtitle: "",
+      logo_path: "redbull.jpg",
+      //certificate_link:
+        //"https://coursera.org/share/cbcd30865e98e2e45161a6ace19d3558",
+      //alt_name: "IBM",
+      color_code: "#096ce1",
+    },
+    {
+      title: "Power BI: Dashboards for beginners",
+      subtitle: "",
+      logo_path: "linkedin.png",
+      //certificate_link:
+        //"https://coursera.org/share/4fa1b8d2f654495a0b692b65d9964ad5",
+      alt_name: "IBM",
+      color_code: "#096ce1",
+    },
+    {
+      title: "Investment Banking Virtual Experience Program",
+      subtitle: "",
+      logo_path: "JP-Morgan.png",
+      //certificate_link:
+        //"https://coursera.org/share/438a8e16af4786782c8a739aef79e999",
+      alt_name: "IBM",
+      color_code: "#096ce1",
+    },
+    {
+      title: "Web Development BootCamp",
+      subtitle: "",
+      logo_path: "ibm_logo.jpg",
+      //certificate_link:
+        //"https://coursera.org/share/cfa7b88b913e4a8446b9dc706c0802b0",
+      alt_name: "IBM",
+      color_code: "#096ce1",
+    },
+    {
+      title: "Learning Data Analytics: 1 Foundations",
+      subtitle: "",
+      logo_path: "linkedin.png",
+      //certificate_link:
+       // "https://coursera.org/share/47d8c39642888094ad458b5e44a3c9f3",
+      alt_name: "IBM",
+      color_code: "#096ce1",
+    },
+    {
+      title: "BlockChain Basics",
+      subtitle: "",
+      logo_path: "linkedin.png",
+      //certificate_link:
+       // "https://coursera.org/share/f4ce59c0d68e4569056faaf2fdb4c256",
+      alt_name: "IBM",
+      color_code: "#096ce1",
+    },
+    {
+      title: "Product Led Growth Micro-Certification",
+      subtitle: "",
+      logo_path: "product.png",
+      //certificate_link:
+        //"https://coursera.org/share/f80912b58e3afcfa49e6116210a2c9da",
+      alt_name: "Meta",
+      color_code: "#096ce1",
+    },
+    {
+      title: "Summer Military Training Course",
+      subtitle: "",
+      logo_path: "military.jpg",
+      //certificate_link:
+        //"https://coursera.org/share/877acd7eaf9dd532a2cb5b103576967e",
+      alt_name: "Meta",
+      color_code: "#096ce1",
+    },
+    {
+      title: "Spanish A1.1",
+      subtitle: "",
+      logo_path: "multibhashi.png",
+      //certificate_link:
+        //"https://coursera.org/share/6af001cafc4b683bb38abdb9e81d0d79",
+      alt_name: "Duke University",
+      color_code: "#001a57",
+    },
+    {
+      title: "Vishwacon",
+      subtitle: "",
+      logo_path: "viit.jpg",
+      //certificate_link:
+        //"https://coursera.org/share/29c75a387fcaf2f1e2403f73473092b2",
+      alt_name: "Meta",
+      color_code: "#096ce1",
+    },
+  ],
+};
+
+const experience = {
+  title: "Experience",
+  subtitle: "Work and Internships",
+  //description:
+    //"",
+  header_image_path: "experience.svg",
+  sections: {
+    title: "Work Experience",
+    experiences: [
+      {
+        title: "PMO Intern",
+        company: "Philips",
+        company_url: "https://www.philips.co.in/",
+        logo_path: "Phillips.png",
+        alt_name: "Philips",
+        duration: "(Oct 2023 - Present)",
+        location: "Pune/Pimpri-Chincwad Area (On-Site)",
+        description: "<li> Working on developing a media management software to schedule advertisements in the robot screens.</li><br><li>Working on optimizing E-commerce app and developing analytics dashboard for robots and orders.</li><br><li>Implemented socket to allow users to transfer the controlling of a robot to a different user.</li><br><li>Developed a dashboard to display live feed from robot with map, joystick controls to control the robot with robot commands.</li>",
+        color: "#000000",
+      },
+      {
+        title: "Enterpreneurship Development Cell ,VIIT",
+        company: "VIIT",
+        company_url: "https://edcviit.com/",
+        logo_path: "edc.jpg",
+        alt_name: "edc",
+      duration: "(March 2021 - Present)",
+        location: "VIIT Pune",
+        description: "<li>  Managed the team along with juniors.</li><br><li>Successfully Implemented various initaiatives to help students.</li><br><li>Actively participated in various Activities</li><br>",
+        color: "#0071C5",
+      },
+      {
+        title: "Data Analyst",
+        company: "Jio Platforms Limited",
+        company_url: "https://www.jio.com/platforms/",
+        logo_path: "jio.png",
+        alt_name: "infosys-logo",
+      duration: "(Aug 2023  - Oct 2023)",
+        location: "Navi Mumbai(On-Site)",
+        description: "<li> Created a PWA for educators, allowing them to create analogies with animations using HTML, CSS, JavaScript, and Fabric.js.</li><br><li>Enhanced the product, introduced new features like multiple content pages, audio, custom gradients, trigger-based animations using React.js.</li><br><li>Developed an web based AR/VR learning platform using Node, Express and socket for multi-user experience and other features like chat, screen, audio and video share for web based.</li><br><li>Developed 2 new tools with an intuitive design for interactive content creation using Angular</li>",
+        color: "#0071C5",
+      },
+      {
+        title: "Business Development Analyst",
+        company: "Sanspots",
+        company_url: "https://www.infosys.com/",
+        logo_path: "sanspots.png",
+        alt_name: "sanspots",
+      duration: "(Oct 2022 - Nov 2022)",
+        location: "Pune",
+        description: "<li> Created a PWA for educators, allowing them to create analogies with animations using HTML, CSS, JavaScript, and Fabric.js.</li><br><li>Enhanced the product, introduced new features like multiple content pages, audio, custom gradients, trigger-based animations using React.js.</li><br><li>Developed an web based AR/VR learning platform using Node, Express and socket for multi-user experience and other features like chat, screen, audio and video share for web based.</li><br><li>Developed 2 new tools with an intuitive design for interactive content creation using Angular</li>",
+        color: "#0071C5",
+      }
+    ],
+  },
+};
+
+
+const projectsHeader = {
+  title: "Projects",
+  //description:
+    //",
+  avatar_image_path: "projects_image.svg",
+};
+
+const projects = {
+  data: [
+    {
+      id: "project-0",
+      name: "Flight Schedule using Adjacency List in Graph",
+      //url: "https://crafty-commerce.vercel.app/",
+      repoUrl: "#",
+      description:
+        "Implemented the program using C++ for scheduling flight travel . It tells users to schedule flight for gibven specified locations in organised manner.",
+      languages: [
+        // {
+        //   name: "React",
+        //   iconifyClass: "logos:react",
+        // },
+        // {
+        //   name: "C++",
+        //   iconifyClass: "logos:C++",
+        // },
+        // {
+        //   name: "Express",
+        //   iconifyClass: "simple-icons:express",
+        // },
+        // {
+        //   name: "Nodejs",
+        //   iconifyClass: "logos:nodejs",
+        // },
+        // {
+        //   name: "MongoDB",
+        //   iconifyClass: "vscode-icons:file-type-mongo",
+        // },
+        // {
+        //   name: "Stripe",
+        //   iconifyClass: "logos:stripe",
+        // },
+        // {
+        //   name: "Tailwind",
+        //   iconifyClass: "vscode-icons:file-type-tailwind",
+        // },
+        // {
+        //   name: "Cloudinary",
+        //   iconifyClass: "logos:cloudinary-icon",
+        // },
+        // {
+        //   name: "Framer Motion",
+        //   iconifyClass: "logos:framer",
+        // },
+      ],
+    },
+    {
+      id: "project-1",
+      name: "Save-A-Thon",
+      url: "",
+      repoUrl: "#",
+      description:
+        "Created a website which can act as a  platform or medium to transfer waste generated form societies to various factories or agencies",
+      languages: [
+        // {
+        //   name: "React",
+        //   iconifyClass: "logos:react",
+        // },
+        // {
+        //   name: "Redux",
+        //   iconifyClass: "logos:redux",
+        // },
+        // {
+        //   name: "Firebase",
+        //   iconifyClass: "logos:firebase",
+        // },
+        // {
+        //   name: "Tailwind",
+        //   iconifyClass: "vscode-icons:file-type-tailwind",
+        // },
+        // {
+        //   name: "Framer Motion",
+        //   iconifyClass: "logos:framer",
+        // },
+      ],
+    },
+    {
+      id: "project-2",
+      name: "Voice Controlled AI Robot",
+      url: "",
+      repoUrl: "#",
+      description:
+        "Designed and Implemented Voice Controlled AI Robot with human interaction ability which may help to lower the burden in coding and can help in various functions",
+    languages: [
+        // {
+        //   name: "Angular",
+        //   iconifyClass: "logos:angular-icon",
+        // },
+        // {
+        //   name: "Express",
+        //   iconifyClass: "simple-icons:express",
+        // },
+        // {
+        //   name: "Nodejs",
+        //   iconifyClass: "logos:nodejs",
+        // },
+        // {
+        //   name: "MongoDB",
+        //   iconifyClass: "vscode-icons:file-type-mongo",
+        // },
+        // {
+        //   name: "Bootstrap",
+        //   iconifyClass: "logos:bootstrap",
+        // },
+        // {
+        //   name: "Handlebars",
+        //   iconifyClass: "vscode-icons:file-type-handlebars",
+        // },
+      ],
+    },
+    {
+      id: "project-3",
+      name: "PyGame",
+      url: "",
+      repoUrl: "#",
+      description: "Created Python Program which allows player to choose a option either to bat or ball and opposite is assigned to computer",
+      languages: [
+        // {
+        //   name: "HTML5",
+        //   iconifyClass: "vscode-icons:file-type-html",
+        // },
+        // {
+        //   name: "CSS3",
+        //   iconifyClass: "vscode-icons:file-type-css",
+        // },
+        // {
+        //   name: "JavaScript",
+        //   iconifyClass: "logos-javascript",
+        // },
+        // {
+        //   name: "React",
+        //   iconifyClass: "logos-react",
+        // },
+      ],
+    },
+    {
+      id: "project-4",
+      name: "Racing Car Game",
+      url: "",
+      repoUrl: "#",
+      description: "Created Car Racing game using C++ graphics ",
+      languages: [
+        // {
+        //   name: "React",
+        //   iconifyClass: "logos-react",
+        // },
+        // {
+        //   name: "HTML5",
+        //   iconifyClass: "vscode-icons:file-type-html",
+        // },
+        // {
+        //   name: "CSS3",
+        //   iconifyClass: "vscode-icons:file-type-css",
+        // },
+        // {
+        //   name: "JavaScript",
+        //   iconifyClass: "logos-javascript",
+        // },
+        // {
+        //   name: "React Reveal",
+        //   iconifyClass: "simple-icons:scrollreveal",
+        // },
+        // {
+        //   name: "Styled Components",
+        //   iconifyClass: "vscode-icons:file-type-styled",
+        // },
+      ],
+    },
+   
+  ],
+};
+
+
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "portfolio.jpg",
+    //description:
+      //",
+  },
+};
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
