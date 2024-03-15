1b9fa2298079d9d2e755540491e18785a14647c5
rajnandini1900 <tanmayc2001@gmail.com>
2024-03-15 23:15:53 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { render } from 'react-dom'
 import ReactDOM from "react-dom/client";
 import { BaseProvider, LightTheme } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
@@ -10,14 +11,12 @@ import "./assets/font-awesome/css/all.css";
 import App from "./App";
 
 const engine = new Styletron();
+const root = document.getElementById("root");
+render( <React.Fragment>
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <App />
+    </BaseProvider>
+  </StyletronProvider>
+</React.Fragment>, root);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.Fragment>
-    <StyletronProvider value={engine}>
-      <BaseProvider theme={LightTheme}>
-        <App />
-      </BaseProvider>
-    </StyletronProvider>
-  </React.Fragment>
-);
